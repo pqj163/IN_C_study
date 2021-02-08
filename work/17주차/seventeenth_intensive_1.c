058e93dcbaceb164e24a091aa5925abd0bbad837
pqj163 <58128948+pqj163@users.noreply.github.com>
2021-02-08 14:53:29 +0900

    17주차

---
+++ work/17주차/seventeenth_intensive_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char replace(char str[], char *s, char *ns);
+
+int main()
+{
+
+    return 0;
+}
+
+char replace(char str[], char *s, char *ns)
+{
+}
